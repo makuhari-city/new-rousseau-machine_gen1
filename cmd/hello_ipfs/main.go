96de484b0bd2f57a0d54061ac77d57f548f234a7
fujihira@archiroid.com <fujihira@archiroid.com>
2021-06-20 10:00:01 +0900

    ipfs publish study

--- cmd/hello_ipfs/main.go
+++ cmd/hello_ipfs/main.go
@@ -1,19 +1,33 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"strings"
-	"os"
 	"io/ioutil"
-	"net/http"
 	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"time"
+
 	shell "github.com/ipfs/go-ipfs-api"
 )
 
+type IpfsData struct {
+	CID     string `json:"cid"`
+	Content string `json:"content"`
+}
+
+type PublishResponse struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintln(w, "Hello, world.")
-    })
+		fmt.Fprintln(w, "Hello, ipfs api.")
+	})
 
 	http.HandleFunc("/post", func(w http.ResponseWriter, r *http.Request) {
 		bodyBytes, err := ioutil.ReadAll(r.Body)
@@ -21,27 +35,131 @@ func main() {
 			log.Fatal(err)
 		}
 		bodyString := string(bodyBytes)
+		fmt.Printf("post content %s\n", bodyString)
 		fmt.Fprintln(w, bodyString)
-    })
+	})
 
-	http.HandleFunc("/ipfs/add", func(w http.ResponseWriter, r *http.Request) {
-		bodyBytes, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-		bodyString := string(bodyBytes)
-
+	http.HandleFunc("/ipfs", func(w http.ResponseWriter, r *http.Request) {
 		// Where your local node is running on localhost:5001
 		sh := shell.NewShell("ipfs0:5001")
-		cid, err := sh.Add(strings.NewReader(bodyString))
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "error: %s", err)
-			os.Exit(1)
+		sh.SetTimeout(1 * time.Second)
+
+		switch r.Method {
+		case "PUT":
+			url_target := "http://ipfs0:5001/api/v0/name/publish?arg=%2Fipfs%2FQmPATpRyHsRxDkDQtLzpoDKGe3Dt7icWSz2THWjQMt3Xx7&lifetime=100h0m0s&resolve=false&ttl=1m0s"
+			args := url.Values{}
+			// args.Add("arg", "/ipfs/QmPATpRyHsRxDkDQtLzpoDKGe3Dt7icWSz2THWjQMt3Xx7")
+			// args.Add("lifetime", "1m0s")
+			// args.Add("resolve", "false")
+			// args.Add("ttl", "1m0s")
+			res, err := http.PostForm(url_target, args)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Request error: %s", err)
+				return
+			}
+			defer res.Body.Close()
+
+			body, err := ioutil.ReadAll(res.Body)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Request error: %s", err)
+				return
+			}
+
+			// str_json := string(body)
+			// fmt.Println(str_json)
+
+			var pubResp PublishResponse
+			if err := json.Unmarshal(body, &pubResp); err != nil {
+				fmt.Fprintf(os.Stderr, "json error: %s", err)
+				return
+			}
+
+			fmt.Fprintln(w, pubResp)
+
+			// req := sh.Request("name/publish", "/ipfs/"+"QmPATpRyHsRxDkDQtLzpoDKGe3Dt7icWSz2THWjQMt3Xx7").Option("resolve", false)
+			// req.Option("lifetime", time.Hour*100)
+			// req.Option("ttl", time.Second*60)
+			// // req.Option("timeout", time.Second*60)
+
+			// var pubResp PublishResponse
+			// ctx := context.Background()
+			// ctx, cancel := context.WithTimeout(ctx, 60*time.Second)
+			// fmt.Printf("cancel %s\n", cancel)
+
+			// err := req.Exec(ctx, &pubResp)
+			// if err != nil {
+			// 	log.Fatal(err)
+			// 	return
+			// }
+
+			// resp, err := sh.PublishWithDetails("/ipfs/"+"QmR2HwcTCTD5iBCkixbth1mgHJcLCdQxwJPEd15et5gSgw", "", time.Hour*100, time.Second*60, false)
+			// if err != nil {
+			// 	log.Fatal(err)
+			// 	return
+			// }
+			// fmt.Printf("sub %s\n", pubResp)
+
+			// err = sh.Publish("12D3KooWD1PhECTxaZzA5e3bg89z2dQAnm41BjZDKoT6K323UbFx", bodyString)
+
+			// var topic = "makuhari-vote-list"
+			// sub, err := sh.PubSubSubscribe(topic)
+			// time.Sleep(10 * time.Millisecond)
+			// if err != nil {
+			// 	log.Fatal(err)
+			// 	return
+			// }
+			// fmt.Printf("sub %s\n", sub)
+
+			// // err = sh.PubSubPublish(topic, bodyString)
+
+			// r, err := sub.Next()
+			// fmt.Printf("Data %s\n", r.Data)
+
+			// sub.Cancel()
+		case "POST":
+			bodyBytes, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				log.Fatal(err)
+				return
+			}
+			bodyString := string(bodyBytes)
+
+			// // decode to structure
+			// var ipfsData IpfsData
+			// if err := json.Unmarshal(bodyBytes, &ipfsData); err != nil {
+			// 	log.Fatal(err)
+			// 	return
+			// }
+
+			fmt.Println("post content", bodyString)
+			// cid, err := sh.Add(strings.NewReader(newStr))
+			cid, err := sh.Add(bytes.NewBufferString(bodyString))
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "error: %s", err)
+				return
+			}
+			fmt.Println("added ", cid)
+			fmt.Fprintln(w, cid)
+		case "GET":
+			var cid = r.FormValue("cid")
+			fmt.Println("Cat ", cid)
+			reader, err := sh.Cat(cid)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "error: %s", err)
+				return
+			}
+			buf := new(bytes.Buffer)
+			buf.ReadFrom(reader)
+			catContent := buf.String()
+			fmt.Println("catContent ", catContent)
+			fmt.Fprintln(w, catContent)
+
+		default:
+			return
 		}
-		fmt.Printf("added %s", cid)
-		fmt.Fprintln(w, cid)
-    })
+
+	})
 
 	http.ListenAndServe(":8080", nil)
-	
+
 }
